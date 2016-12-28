9e46796019a14f2fc6cca3fe9438b53729efda97
svaka1 <svakacharla@sapient.com>
2016-12-28 13:15:20 +0530

    registered and injected product service in product-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import {ProductService} from './products/product.service';
 @Component({
     selector: 'pm-app',
     template: `
         <div><h3>{{pageTitle}}</h3>
            <pm-products></pm-products>
         </div>   
-    `
+    `,
+    providers:[ProductService]
 })
 export class AppComponent { 
     pageTitle: string = 'Product Management'
